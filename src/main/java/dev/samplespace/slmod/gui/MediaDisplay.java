0672a37d4fc84b14569c75c0f5ec5a5d90b2e2c1
SampleSpaceDev <sam@samplespace.dev>
2023-05-09 17:18:15 +0100

    Optimise imports

--- src/main/java/dev/samplespace/slmod/gui/MediaDisplay.java
+++ src/main/java/dev/samplespace/slmod/gui/MediaDisplay.java
@@ -9,7 +9,6 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.ChatScreen;
-import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
